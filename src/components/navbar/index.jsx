a18669c675525e94441061c05fff0b72955ffb04
Adriel Abner Aparecido <adriel5510@hotmail.com>
2024-05-23 08:50:16 -0300

    Finalizado projeto responsive-menu

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,17 +1,29 @@
+import { FaBars } from "react-icons/fa";
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
+import { FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState("navbar");
+
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <a href="/">Inicio</a>
-          <a href="/sobre">Sobre</a>
-          <a href="/contato">Contato</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={`${toggle}`}>
+        <a className="btn-close" onClick={() => setToggle("navbar")}>
+          <FaXmark />
+        </a>
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+            <a href="/sobre">Sobre</a>
+            <a href="/contato">Contato</a>
+          </li>
+        </ul>
+      </div>
+      <a className="btn-toggle" onClick={() => setToggle("toggle")}>
+        <FaBars />
+      </a>
+    </>
   );
 };
 export default Navbar;
